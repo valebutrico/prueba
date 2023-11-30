177dbf8c277b072216d508da546397f4a1cc565c
butrovich <135270399+butrovich@users.noreply.github.com>
2023-11-30 14:31:32 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MostrarUsuario />
+      <MostrarUsuario />
     </div>
   );
+}
+
+
+const MostrarUsuario = () => {
+  const name = <h1>Vale Butrico</h1>;
+  const age = <p>26</p>;
+  const email = <p>vale.butrico@gmail.com</p>;
+
+  return <div>{name} {age} {email}</div>;
 }
 
 export default App;
